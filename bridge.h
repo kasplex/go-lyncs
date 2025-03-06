7127dacb19a87e275f1121be754088b2ac6e397b
5bb55b <5bb55b@kasplex.org>
2025-03-06 17:35:09 +0000

    stateSandbox

--- bridge.h
+++ bridge.h
@@ -1,25 +1,10 @@
 #include <string.h>
 
 ////////////////////////////////
-__attribute__((weak))
-int _g_mt_newindex(lua_State *s) {
-    const char *key = lua_tostring(s, 2);
-    const char *allowed[] = {"session", "init", "regst", "run", NULL};
-    for (int i=0; allowed[i]!=NULL; i++) {
-        if (strcmp(key,allowed[i])==0) {
-            lua_rawset(s, 1);
-            return 0;
-        }
-    }
-    luaL_error(s, "variable read-only");
-    return 0;
-}
-
-////////////////////////////////
-__attribute__((weak))
+/*__attribute__((weak))
 int _mt_newindex(lua_State *s) {
     luaL_error(s, "variable read-only");
     return 0;
-}
+}*/
 
 // ...
--- state.go
+++ state.go
@@ -23,10 +23,16 @@ import (
 var stateRemoveMap = map[string][]string{
     "string": {"dump"},
     "math": {"randomseed","random"},
-    "_G": {"jit","collectgarbage","getfenv","setfenv","rawget","rawset","loadfile","load","loadstring","dofile","gcinfo","coroutine","debug"/*,"print"*/},
+    "_G": {"jit","collectgarbage","rawget","rawset","loadfile","load","loadstring","dofile","gcinfo","coroutine","debug","getfenv"/*,"print"*/},
 }
 
-var stateReadonlyList = []string{"_G","table","string","math","bit"/*,built-in..*/}
+var stateReadonlyList = `
+    _G.table = _set(table)
+    _G.string = _set(string)
+    _G.math = _set(math)
+    _G.bit = _set(bit)
+    -- built-in ...
+`
 
 ////////////////////////////////
 func stateFromCode(code string) (*C.lua_State, error) {
@@ -34,22 +40,17 @@ func stateFromCode(code string) (*C.lua_State, error) {
     if s == nil {
         return nil, fmt.Errorf("creation failed @stateFromCode")
     }
-    stateLibOpen(s)
-    cCode := C.CString(code)
+    err := stateSandbox(s, true)
+    if err != nil {
+        stateClose(s)
+        return nil, err
+    }
+    cCode := C.CString("setRO();" + code)
     defer C.free(unsafe.Pointer(cCode))
     if C.LUA_OK != C.luaL_loadstring(s, cCode) {
         stateClose(s)
         return nil, fmt.Errorf("load failed @stateFromCode")
     }
-    err := stateLibFilter(s)
-    if err != nil {
-        stateClose(s)
-        return nil, err
-    }
-    
-    // builtin ...
-    
-    stateSetGlobalReadonly(s)
     err = stateCall(s)
     if err != nil {
         stateClose(s)
@@ -57,7 +58,7 @@ func stateFromCode(code string) (*C.lua_State, error) {
     }
     
     // check sc-func exists ...
-    // dump ...
+    // buffer dump ...
     
     return s, nil
 }
@@ -68,28 +69,22 @@ func stateFromBuffer(buffer []byte) (*C.lua_State, error) {
     if s == nil {
         return nil, fmt.Errorf("creation failed @stateFromBuffer")
     }
-    stateLibOpen(s)
-    
-    // load buffer ...
-
-    err := stateLibFilter(s)
+    err := stateSandbox(s, false)
     if err != nil {
         stateClose(s)
         return nil, err
     }
-
-    // builtin ...
     
-    stateSetGlobalReadonly(s)
-    
-    // pcall ...
+    // load buffer ...    
+    // pcall ...    
     // check sc-func exists ...
     
     return s, nil
 }
 
 ////////////////////////////////
-func stateLibOpen(s *C.lua_State) {
+func stateSandbox(s *C.lua_State, setRO bool) (error) {
+    var err error
     C.luaopen_base(s)
     C.luaopen_math(s)
     C.luaopen_string(s)
@@ -97,11 +92,6 @@ func stateLibOpen(s *C.lua_State) {
     C.luaopen_bit(s)
     C.luaopen_jit(s)
     C.luaopen_string_buffer(s)
-}
-
-////////////////////////////////
-func stateLibFilter(s *C.lua_State) (error) {
-    var err error
     for k, v := range stateRemoveMap {
         err = stateSetTableFieldNil(s, k, v)
         if err != nil {
@@ -109,6 +99,49 @@ func stateLibFilter(s *C.lua_State) (error) {
         }
     }
     stateSetTableFieldString(s, "_G", []string{"_VERSION"}, []string{"Lua 5.1 Lyncs"})
+    
+    // builtin ...
+    
+    if setRO {
+        cCode := C.CString(`
+function setRO()
+    local _set = function (t)
+        local p = {}
+        local mt = {
+            __index = t,
+            __newindex = function(_, k, v)
+                if t ~= _G then
+                    error("variable read-only", 2)
+                end
+                if k=="session" then
+                    t[k] = v
+                    return
+                end
+                if (k=="init" or k=="run") and t[k]==nil and type(v)=="function" then
+                    t[k] = v
+                    return
+                end
+                error("variable read-only", 2)
+            end
+        }
+        setmetatable(p, mt)
+        return p
+    end
+` + stateReadonlyList + `
+    setfenv(2, _set(_G))
+    _G.setfenv = nil
+    _G.setRO = nil
+end
+        `)
+        defer C.free(unsafe.Pointer(cCode))
+        if C.LUA_OK != C.luaL_loadstring(s, cCode) {
+            return fmt.Errorf("load failed @stateSandbox")
+        }
+        err = stateCall(s)
+        if err != nil {
+            return err
+        }
+    }
     return nil
 }
 
@@ -118,8 +151,11 @@ func stateCall(s *C.lua_State) (error) {
         msg := C.GoString(C.lua_tolstring(s, -1, nil))
         C.lua_settop(s, C.lua_gettop(s)-1)
         msgS := strings.Split(msg, `"]:`)
-        if len(msgS[1]) > 40 {
-            msgS[1] = msgS[1][:40] + ".."
+        if len(msgS) < 2 {
+            return fmt.Errorf(msg + " @stateCall")
+        }
+        if len(msgS[1]) > 30 {
+            msgS[1] = msgS[1][:30] + ".."
         }
         return fmt.Errorf(msgS[1] + " @stateCall")
     }
@@ -136,35 +172,6 @@ func stateClean(s *C.lua_State) {
 
     // ...
     
-}
-
-////////////////////////////////
-func stateSetTableReadonly(s *C.lua_State, table string) {
-    ct := C.CString(table)
-    defer C.free(unsafe.Pointer(ct))
-    ctRo := C.CString(table+"_ro")
-    defer C.free(unsafe.Pointer(ctRo))
-    cf := C.CString("__newindex")
-    defer C.free(unsafe.Pointer(cf))
-    C.luaL_newmetatable(s, ctRo)
-    C.lua_pushstring(s, cf)
-    if table == "_G" {
-        C.lua_pushcclosure(s, C.lua_CFunction(C._g_mt_newindex), 0)
-    } else {
-        C.lua_pushcclosure(s, C.lua_CFunction(C._mt_newindex), 0)
-    }
-    C.lua_settable(s, -3)
-    C.lua_getfield(s, C.LUA_GLOBALSINDEX, ct)
-    C.lua_pushvalue(s, -2)
-    C.lua_setmetatable(s, -2)
-    C.lua_settop(s, C.lua_gettop(s)-2)
-}
-
-////////////////////////////////
-func stateSetGlobalReadonly(s *C.lua_State) {
-    for _, t := range stateReadonlyList {
-        stateSetTableReadonly(s, t)
-    }
 }
 
 ////////////////////////////////
