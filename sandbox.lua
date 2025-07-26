0d13fbb8090c41ffbcf1cd44114ec4ecbdb7a865
5bb55b <5bb55b@kasplex.org>
2025-07-26 12:51:40 +0000

    minor changes

--- sandbox.lua
+++ sandbox.lua
@@ -11,8 +11,8 @@
 			__index=t,
 			__newindex=function(_, k, v)
 				if t~=_G_RAW then error("variable read-only "..k,2) end
-				if fn[k] and t[k]==nil and type(v)=="function" then t[k]=v; return end
 				if k=="session" or k=="state" then t[k]=v; return end
+				if fn[k] and t[k]==nil and type(v)=="function" then t[k]=v; return end
 				error("variable read-only "..k, 2)
 			end
 		}
